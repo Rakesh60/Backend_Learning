1193d9c4fa68f75fdf7850f297de688aaa10934c
Rakesh60 <rakeshhupta5353@gmail.com>
2024-01-31 17:27:42 +0530

    apis created

--- index.js
+++ index.js
@@ -3,15 +3,28 @@ const http =require('http');
 const fs=require('fs')
 
 const index=fs.readFileSync('index.html','utf-8')
+const data=fs.readFileSync('data.json','utf-8')
 
 
-const data={age:5}
 const server=http.createServer((req,res)=>{
-    console.log(req.url)
-    console.log('Server Started')
-    res.setHeader('Dummy','DummyValue')
-    res.setHeader('Content-Type','text/html')
-    res.end(index)
+    switch(req.url){
+        case '/': res.setHeader('Content-Type','text/html');
+        res.end(index);
+        break;
+        case '/api':res.setHeader('Content-Type','application/json')
+        res.end(data);
+        break;
+        default:
+            res.writeHead(404,'Page Not Found');
+            res.end();
+
+
+    }
+    // console.log(req.url)
+    // console.log('Server Started')
+    // res.setHeader('Dummy','DummyValue')
+    // res.setHeader('Content-Type','text/html')
+   
 })
 
 server.listen(8080);
