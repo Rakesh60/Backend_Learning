de96118b22c40d3f65e68cec07c99ddad6f42cee
Rakesh60 <rakeshgupta5353@gmail.com>
2024-02-02 13:52:49 +0530

    done

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-const fs=require('fs')
-
+const fs = require("fs");
 
 // const index=fs.readFileSync('index.html','utf-8')
-const data=fs.readFileSync('data.json','utf-8')
- const products=JSON.parse(data)
+const data = fs.readFileSync("data.json", "utf-8");
+const products = JSON.parse(data);
 
-const express =require('express');
+const express = require("express");
 //const { type } = require('os');
-const server=express();
+const server = express();
 // server.get('/',(req,res)=>{
 //     res.send('<h1>Hello World</h1>')
 //     res.sendFile('E:/Backend_Learning/index.html')
@@ -15,48 +14,46 @@ const server=express();
 //    res.sendStatus(404);
 //    res.status(202).send('<h1>Own Status</h1>')
 
-
 // })
 
 // server.get('/demo',(req,res)=>{
 
 // })
 
-
 // server.use((req,res,next)=>{
-    
+
 //     console.log(req.method,req.ip,req.hostname,req.get('User-Agent'))
 //     next();
 // })
-const auth=(req,res,next)=>{
-    if (req.query.password=='123') {
-        next();
-        
-    } else {
-        res.send(401);
-    }
-}
+server.use(express.json());
+//server.use(express.static("public"));
+const auth = (req, res, next) => {
+  //   if (req.body.password == "123") {
+  //     next();
+  //   } else {
+  //     res.sendStatus(401);
+  //   }
+  next();
+};
 
-server.get('/',auth,(req,res)=>{
-    res.json({type:'GET'})
-    //console.log(req.header)
-})
-server.put('/',auth,(req,res)=>{
-    res.json({type:'PUT'})
-})
-server.post('/',(req,res)=>{
-    res.json({type:'POST'})
-})
-server.delete('/',(req,res)=>{
-    res.json({type:'DELETE'})
-})
-server.patch('/',(req,res)=>{
-    res.json({type:'PATCH'})
-})
+server.get("/products/:id", auth, (req, res) => {
+  console.log(req.params);
+  res.json({ type: "GET" });
+  //console.log(req.header)
+});
+server.put("/", auth, (req, res) => {
+  res.json({ type: "PUT" });
+});
+server.post("/", auth, (req, res) => {
+  res.json({ type: "POST" });
+});
+server.delete("/", (req, res) => {
+  res.json({ type: "DELETE" });
+});
+server.patch("/", (req, res) => {
+  res.json({ type: "PATCH" });
+});
 
-
-
-
-server.listen(8000,()=>{
-    console.log('Server Started')
-})
+server.listen(8000, () => {
+  console.log("Server Started");
+});
