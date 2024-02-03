ac703bb125b921ac512a88ded46e61524f8a0225
Rakesh60 <rakeshgupta5353@gmail.com>
2024-02-03 11:10:09 +0530

    crud api's

--- index.js
+++ index.js
@@ -1,59 +1,72 @@
 const fs = require("fs");
-
-// const index=fs.readFileSync('index.html','utf-8')
-const data = fs.readFileSync("data.json", "utf-8");
-const products = JSON.parse(data);
+const index = fs.readFileSync("index.html", "utf-8");
+const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const products = data.products;
 
 const express = require("express");
-//const { type } = require('os');
+const morgan = require("morgan");
 const server = express();
-// server.get('/',(req,res)=>{
-//     res.send('<h1>Hello World</h1>')
-//     res.sendFile('E:/Backend_Learning/index.html')
-//    res.json(products)
-//    res.sendStatus(404);
-//    res.status(202).send('<h1>Own Status</h1>')
 
-// })
-
-// server.get('/demo',(req,res)=>{
-
-// })
-
-// server.use((req,res,next)=>{
-
-//     console.log(req.method,req.ip,req.hostname,req.get('User-Agent'))
-//     next();
-// })
+//bodyParser
 server.use(express.json());
-//server.use(express.static("public"));
-const auth = (req, res, next) => {
-  //   if (req.body.password == "123") {
-  //     next();
-  //   } else {
-  //     res.sendStatus(401);
-  //   }
-  next();
-};
+server.use(morgan("default"));
+server.use(express.static("public"));
 
-server.get("/products/:id", auth, (req, res) => {
-  console.log(req.params);
-  res.json({ type: "GET" });
-  //console.log(req.header)
-});
-server.put("/", auth, (req, res) => {
-  res.json({ type: "PUT" });
-});
-server.post("/", auth, (req, res) => {
-  res.json({ type: "POST" });
-});
-server.delete("/", (req, res) => {
-  res.json({ type: "DELETE" });
-});
-server.patch("/", (req, res) => {
-  res.json({ type: "PATCH" });
+// API - Endpoint - Route
+
+// Products
+// API ROOT , base URL, example - google.com/api/v2/
+
+//Create POST /products     C R U D
+server.post("/products", (req, res) => {
+  console.log(req.body);
+  products.push(req.body);
+  res.status(201).json(req.body);
 });
 
-server.listen(8000, () => {
-  console.log("Server Started");
+// Read GET /products
+server.get("/products", (req, res) => {
+  res.json(products);
+});
+
+// Read GET /products/:id
+server.get("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const product = products.find((p) => p.id === id);
+  res.json(product);
+});
+
+// Update PUT /products/:id
+server.put("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  products.splice(productIndex, 1, { ...req.body, id: id });
+  res.status(201).json();
+});
+// Update PATCH /products/:id
+server.patch("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  const product = products[productIndex];
+  products.splice(productIndex, 1, { ...product, ...req.body });
+  res.status(201).json();
+});
+// Delete DELETE /products/:id
+server.delete("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex((p) => p.id === id);
+  const product = products[productIndex];
+  products.splice(productIndex, 1);
+  res.status(201).json(product);
+});
+
+server.get("/demo", (req, res) => {
+  // res.sendStatus(404);
+  // res.json(products)
+  // res.status(201).send('<h1>hello</h1>')
+  // res.sendFile('/Users/abhishekrathore/Desktop/node-app/index.html')
+});
+
+server.listen(8080, () => {
+  console.log("server started");
 });
