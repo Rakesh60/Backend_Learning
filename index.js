f6fcc15465a9485b5a76b79b13d7b83bb02fddc2
Rakesh60 <rakeshgupta5353@gmail.com>
2024-02-10 02:39:31 +0530

    user schema created

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const cors=require('cors');
-const path=require('path')
+const cors = require("cors");
+const path = require("path");
 
 const server = express();
 const productRouter = require("./routes/product");
@@ -18,22 +18,16 @@ async function main() {
   console.log("Data Base Connected");
 }
 
-
-
-
-
-
-
 //bodyParser
-server.use(cors()) //cross origin
+server.use(cors()); //cross origin
 server.use(express.json());
 server.use(morgan("default"));
-server.use(express.static(path.resolve(__dirname,process.env.PUBLIC_DIR)));
+server.use(express.static(path.resolve(__dirname, process.env.PUBLIC_DIR)));
 server.use("/products", productRouter.router);
-server.use("/user", userRouter.router);
-server.use('*',(req,res)=>{
-  res.sendFile(path.resolve(__dirname,'build','index.html'))
-})
+server.use("/users", userRouter.router);
+server.use("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
 
 server.listen(process.env.PORT, () => {
   console.log("server started");
